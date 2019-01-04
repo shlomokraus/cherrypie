c5394d678fa3a5120a74fdb1e629d04450f8d522
Shlomo Kraus <plonit7@gmail.com>
2019-01-04 18:19:52 +0200

    feat: replace native svg with octicons react module

--- src/components/Execute.tsx
+++ src/components/Execute.tsx
@@ -5,6 +5,7 @@ import { useGlobalState } from "../context/GlobalState";
 import { useSlice } from "../hooks/cherry";
 import { ProcessStatus } from "../constants";
 import { useGlobalStore, dispatch } from "../context/GlobalStore";
+import Octicon, {Check, X, MarkGithub} from '@githubprimer/octicons-react'
 
 export const Execute = props => {
   const { slices, setSlices } = useSlices();
@@ -49,13 +50,13 @@ export const Execute = props => {
       <RenderStatus status={status} error={error} />
       <div style={{ minHeight: "300px" }}>
         {messages.map((message, index) => {
-          let Icon = <CheckIcon />;
+          let Icon = <Octicon  width="16" icon={Check} />;
           const isLast = index === messages.length - 1;
           if (isLast) {
             if (status === ProcessStatus.Failed) {
-              Icon = <CrossIcon />;
+              Icon = <Octicon  width="16" icon={X} />;
             } else if (status === ProcessStatus.Working) {
-              Icon = <PulseIcon />;
+              Icon = <Octicon width="16" className="octicon octicon-mark-github anim-pulse" icon={MarkGithub} />;
             }
           }
 
@@ -132,55 +133,4 @@ const RenderStatus = ({ status, error }) => {
     default:
       return <div />;
   }
-};
-
-const PulseIcon = () => {
-  return (
-    <svg
-      height="16"
-      className="octicon octicon-mark-github anim-pulse"
-      viewBox="0 0 16 16"
-      version="1.1"
-      width="16"
-      aria-hidden="true"
-    >
-      <path
-        fillRule="evenodd"
-        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-      />
-    </svg>
-  );
-};
-
-const CheckIcon = () => {
-  return (
-    <svg
-      height="16"
-      width="16"
-      className="octicon octicon-check d-block mx-auto"
-      viewBox="0 0 12 16"
-      version="1.1"
-      aria-hidden="true"
-    >
-      <path fillRule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z" />
-    </svg>
-  );
-};
-
-const CrossIcon = () => {
-  return (
-    <svg
-      height="16"
-      width="16"
-      className="octicon octicon-x d-block mx-auto"
-      viewBox="0 0 12 16"
-      version="1.1"
-      aria-hidden="true"
-    >
-      <path
-        fillRule="evenodd"
-        d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z"
-      />
-    </svg>
-  );
 };
